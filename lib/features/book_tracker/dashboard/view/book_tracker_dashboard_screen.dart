82c758ac966a7dd2cd7050bf1a867bf6710a23de
mnauman2008 <106969639+mnauman2008@users.noreply.github.com>
2025-07-22 18:55:33 +0500

    indicator

--- lib/features/book_tracker/dashboard/view/book_tracker_dashboard_screen.dart
+++ lib/features/book_tracker/dashboard/view/book_tracker_dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../../config/text_styles.dart';
+import '../../../../config/theme.dart';
 import '../../../../shared/widgets/base_screen.dart';
 class BookTrackerDashboardScreen extends StatefulWidget {
   const BookTrackerDashboardScreen({super.key});
@@ -26,14 +27,30 @@ class _BookTrackerDashboardScreenState
   ];
 
   Widget _bottomNavigationBar() {
-    return BottomNavigationBar(
-      items: _items,
-      currentIndex: _currentIndex,
-      onTap: (index) {
-        setState(() {
-          _currentIndex = index;
-        });
-      },
+    final tabCount = 3;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final tabWidth = screenWidth / tabCount;
+    return Stack(
+      children: [
+        BottomNavigationBar(
+          items: _items,
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+        Positioned(
+          top: 0,
+          left: _currentIndex * tabWidth,
+          child: Container(
+            width: tabWidth,
+            height: 3,
+            color: AppColors().primary,
+          ),
+        ),
+      ],
     );
   }
 
