ec878a63398aa2f06070cdac3353c6f6c6b35112
mnauman2008 <106969639+mnauman2008@users.noreply.github.com>
2025-07-27 18:05:07 +0500

    no message

--- lib/features/book_tracker/my_books/view/my_books_screen.dart
+++ lib/features/book_tracker/my_books/view/my_books_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_multi_apps/core/constants/text_constants.dart';
 import 'package:flutter_multi_apps/shared/widgets/base_screen.dart';
 import 'widgets/my_books_tabs.dart';
 import 'widgets/my_books_top_image.dart';
@@ -10,35 +11,11 @@ class MyBooksScreen extends StatefulWidget {
 }
 
 class _MyBooksScreenState extends State<MyBooksScreen> {
-  // Mock data for books list
-  // final List<BookModel> _myBooks = [
-  //   BookModel(
-  //     id: '1',
-  //     title: 'The Great Gatsby',
-  //     authors: ['F. Scott Fitzgerald'],
-  //     coverId: '890',
-  //     status: BookStatus.reading,
-  //   ),
-  //   BookModel(
-  //     id: '2',
-  //     title: 'To Kill a Mockingbird',
-  //     authors: ['Harper Lee'],
-  //     coverId: '890',
-  //     status: BookStatus.wantToRead,
-  //   ),
-  //   BookModel(
-  //     id: '3',
-  //     title: '1984',
-  //     authors: ['George Orwell'],
-  //     coverId: '890',
-  //     status: BookStatus.finished,
-  //   ),
-  // ];
 
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
-      showAppBar: false,
+      title: TextConstants.myBooks,
       child: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) {
           return [const MyBooksTopImage()];
@@ -55,16 +32,4 @@ class _MyBooksScreenState extends State<MyBooksScreen> {
       ),
     );
   }
-
-  // Widget _buildBooksList() {
-  //   return ListView.separated(
-  //     shrinkWrap: true,
-  //     physics: const NeverScrollableScrollPhysics(),
-  //     itemCount: _myBooks.length,
-  //     separatorBuilder: (context, index) => 15.customVerticalSpace,
-  //     itemBuilder: (context, index) {
-  //       return BookListingListCell(book: _myBooks[index]);
-  //     },
-  //   );
-  // }
 }
