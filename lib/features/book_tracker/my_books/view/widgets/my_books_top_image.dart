9462ed20581a37565902ae9b9ace090f7c01e4fc
mnauman2008 <106969639+mnauman2008@users.noreply.github.com>
2025-07-28 05:52:42 +0500

    no message

--- lib/features/book_tracker/my_books/view/widgets/my_books_top_image.dart
+++ lib/features/book_tracker/my_books/view/widgets/my_books_top_image.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_multi_apps/core/utils/app_extensions.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../../config/theme.dart';
 import '../../../../../core/constants/assets_constants.dart';
 import '../../../../../shared/widgets/image_asset.dart';
 
@@ -12,19 +10,9 @@ class MyBooksTopImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverList(
       delegate: SliverChildListDelegate([
-        Container(
+        SizedBox(
           height: 270.h,
           width: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                AppColors().primary,
-                AppColors().primary.customOpacity(0.8),
-              ],
-            ),
-          ),
           child: ImageAsset(
             imagePath: AssetsConstants.booksImage,
             fit: BoxFit.cover,
